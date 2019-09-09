c25cfdb43a3e0c4b436002540e4bae7b7509e90d
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-09 14:33:22 -0600

    get by id and get all accounts endpoints

--- server.js
+++ server.js
@@ -6,4 +6,30 @@ const server = express();
 
 server.use(express.json());
 
+// middleware
+
+
+
+server.get('/accounts', (req, res)=>{
+    db.select('*').from('accounts')
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(error => {
+        res.json(error)
+    })
+})
+
+server.get('/accounts/:id', (req, res)=>{
+    db.select('*').from('accounts').where('id', `${req.params.id}`).first()
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(error => {
+        res.json(error)
+    })
+})
+
+
+
 module.exports = server;
